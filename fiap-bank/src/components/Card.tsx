545867671094e482a27c04ff2e5bd1dede976bc1
Lygia Dias <lygia.dias@moneyp.com.br>
2026-01-20 21:45:15 -0300

    chore: remove src

--- fiap-bank/src/components/Card.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Card({ children, className = "" }: React.PropsWithChildren<{ className?: string }>) {
-  return <div className={"bg-white rounded-2xl p-4 shadow-card " + className}>{children}</div>;
-}
